340f64d1a338c0c0ba294e4ce66c106ee04ee0ee
tommy <tommy.yu@shopeemobile.com>
2018-06-17 00:02:46 +0800

    add watch button

--- statics/js/app.js
+++ statics/js/app.js
@@ -8,11 +8,18 @@ function website_search(){
             for(var i=0;i<data.items.length;i++){
 	        html +="<tr><td>" + (i+1) + "</td>";
 	        html +="<td>"+data.items[i].htmlTitle+"</td>";
-	        html +="<td>"+ data.items[i].link +"</td></tr>";
+	        //html +="<td>"+ data.items[i].link +"</td></tr>";
+	        html +=<'td><button class="btn btn-info mr-sm-1" type="button" onclick="website_watch(\"'+keyword+'''\", \"'+data.items[i].link+'''\",\"'+data.items[i].title+'\")">view</button></td></tr>';
 	    }
 	    $( "#result" ).html(html);
         }
     });
+}
+
+function website_watch(keyword, url, name){
+    console.log(keyword);
+    console.log(url);
+    console.log(name);
 }
 
 $(function(){
