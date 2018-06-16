67c009cd942b7f2ebb78a937dee0b025f23a7665
tommy.yu <lion_awake@163.com>
2018-06-16 16:12:15 +0800

    output searching results to website

--- statics/js/app.js
+++ statics/js/app.js
@@ -1,6 +1,25 @@
-function website.search(){
+function website_search(){
     var keyword = $("#q").val();
-    $.get( "/api/search?keyword"+keyword, function( data ) {
-      $( "#result" ).html( data );
+    $.get( "/api/search?keyword="+keyword, function( data ) {
+      	html = ""
+	if(data.msg != undefined){
+            $("result").html(data.msg)
+        }else{
+            for(var i=0;i<data.items.length;i++){
+	        html +="<tr><td>" + (i+1) + "</td>";
+	        html +="<td>"+data.items[i].htmlTitle+"</td>";
+	        html +="<td>"+ data.items[i].link +"</td></tr>";
+	    }
+	    $( "#result" ).html(html);
+        }
     });
 }
+
+$(function(){
+    $('form').submit(false);  //disable submit of forms
+    $('#q').bind('keypress',function(event){
+        if(event.keyCode == "13"){
+	    website_search();
+	}
+    });
+});
