9e590c64e08c78eb7af5eb00377a0c518c07ccca
tommy <tommy.yu@shopeemobile.com>
2019-04-14 20:59:16 +0800

    fix bug

--- lua/api/mark_job.lua
+++ lua/api/mark_job.lua
@@ -1,6 +1,7 @@
 local video_utils = require("libs.video_utils")
 local response = require("libs.response")
 
+local args = ngx.req.get_uri_args()
 local video_id = args.id
 local secret = args.s
 if secret == '2c81697af5651a4e4be730d02d6b49e4' then
